f5854838727a794506e8041b52e39e1d38f14e33
truongginjs <truongginjs@gmail.com>
2019-09-25 18:52:41 +0700

    commit

---
+++ precache-manifest.37379998a6055ae440abefabde067062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647bc10fd17dcd38d3db0929c85089fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc08eafaebb7f9d1cd14",
+    "url": "/static/css/main.2c76ca98.chunk.css"
+  },
+  {
+    "revision": "81dc2338d8f828a5093c",
+    "url": "/static/js/2.7ccb78ad.chunk.js"
+  },
+  {
+    "revision": "cc08eafaebb7f9d1cd14",
+    "url": "/static/js/main.ace2ff48.chunk.js"
+  },
+  {
+    "revision": "abf6d7114b75447ea20f",
+    "url": "/static/js/runtime~main.02b2333b.js"
+  }
+]);
